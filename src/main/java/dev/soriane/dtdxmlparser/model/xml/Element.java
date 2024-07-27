8d0d21ec49dae9fda1511f0b7a3a6db713263742
Ricardo Soriano <ricardo.roel110@gmail.com>
2024-07-27 11:36:31 -0600

    A model I forgot to commit in the models commit

---
+++ src/main/java/dev/soriane/dtdxmlparser/model/xml/Element.java
@@ -0,0 +1,68 @@
+package dev.soriane.dtdxmlparser.model.xml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Element {
+    /* *
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    *                   ATTRIBUTES                  *
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    * */
+
+    private String name;
+    private List<Element> children;
+    private List<Attribute> attributes;
+    private String content;
+
+    /* *
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    *                  CONSTRUCTOR                  *
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    * */
+
+    public Element (String name) {
+        this.name = name;
+        this.content = null;
+        this.children = new ArrayList<Element>();
+        this.attributes = new ArrayList<>();
+    }
+
+    /* *
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    *              GETTERS AND SETTERS              *
+    * * * * * * * * * * * * * * * * * * * * * * * * *
+    * */
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Element> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Element> children) {
+        this.children = children;
+    }
+
+    public List<Attribute> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(List<Attribute> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
